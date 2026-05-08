5759025bb001a9f165f02896b4bb77e313408e63
Sia <hibari050518@gmail.com>
2026-05-09 03:11:05 +0800

    fix: desktop logo block up 120px paddingBottom, watermark 270px

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -637,8 +637,9 @@ export default function Home() {
           flex:1, display:'flex', flexDirection:'column',
           alignItems:'center', justifyContent:'center', textAlign:'center',
           padding:'0 28%',
+          paddingBottom:'120px',
         }}>
-          <OpeningAnimation style={{ width:'min(580px,44vw)', marginBottom:'-59px', marginTop:'-66px' }} />
+          <OpeningAnimation style={{ width:'min(580px,44vw)', marginBottom:'-59px' }} />
 
           <FadeUp delay={1100}>
             <p style={{
